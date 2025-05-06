c565bfeec7baa614be3980aeaf42e6c26f166287
Adib Hossain <adibhossain1122@outlook.com>
2025-05-06 12:18:05 +0600

    add courses type

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 var users []types.User
+var courses []types.Course
 
 func getUsers(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
@@ -65,6 +66,58 @@ func updateUsers(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func getCourses(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(courses)
+}
+func deleteCourse(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range courses {
+		if item.ID == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(writer).Encode(users)
+}
+
+func getCourseById(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for _, item := range courses {
+		if item.ID == params["id"] {
+			json.NewEncoder(writer).Encode(item)
+			return
+		}
+	}
+}
+func createCourse(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	var course types.Course
+	_ = json.NewDecoder(request.Body).Decode(&course)
+	course.ID = strconv.Itoa(rand.Intn(10000000))
+	courses = append(courses, course)
+	json.NewEncoder(writer).Encode(course)
+
+}
+
+func updateCourse(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(request)
+	for index, item := range courses {
+		if item.ID == params["id"] {
+			courses = append(courses[:index], courses[index+1:]...)
+			var course types.Course
+			_ = json.NewDecoder(request.Body).Decode(&course)
+			course.ID = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(writer).Encode(course)
+			return
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Hello World")
 	r := mux.NewRouter()
@@ -79,11 +132,21 @@ func main() {
 			},
 		},
 	})
+
+	courses = append(courses, types.Course{
+		ID:    "2",
+		Title: "English",
+	})
 	r.HandleFunc("/users", getUsers).Methods("GET")
 	r.HandleFunc("/users/{id}", getUsersById).Methods("GET")
 	r.HandleFunc("/users", createUser).Methods("POST")
 	r.HandleFunc("/users/{id}", updateUsers).Methods("PUT")
 	r.HandleFunc("/users/{id}", deleteUsers).Methods("DELETE")
+	r.HandleFunc("/courses", getCourses).Methods("GET")
+	r.HandleFunc("/courses/{id}", getCourseById).Methods("GET")
+	r.HandleFunc("/courses", createCourse).Methods("POST")
+	r.HandleFunc("/courses/{id}", updateCourse).Methods("PUT")
+	r.HandleFunc("/courses/{id}", deleteCourse).Methods("DELETE")
 
 	fmt.Println("Server started at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
